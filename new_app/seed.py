e212ebc43f24e10581bf397948ff57f9ec46c7df
Race11e <rma23f@fsu.edu>
2026-03-02 13:56:20 -0500

    Add seed.py to new_app/
    This includes functions to update the Cards table. This code has been ran and the Neon Database now has a complete cards table. This should only update the table if there are changes to scryfall data, however this specific feature has not been tested. Functions in this file will be implemented with a github workflow to schedule updates to database automatically.

---
+++ new_app/seed.py
@@ -0,0 +1,77 @@
+# NOTE: A Github workflow will be created to schedule updates to database.
+# Database rows will only be changed if a difference is detected to avoid wasting computing time with Neon.
+# Will change to update PriceHistory (not yet made)
+
+import requests
+from app import app, db, Card
+from sqlalchemy.dialects.postgresql import insert
+
+def fetch_scryfall_data():
+    print("Fetching scryfall data...")
+    bulk_data_info = requests.get('https://api.scryfall.com/bulk-data').json()
+    default_cards_url = next(item['download_uri'] for item in bulk_data_info['data'] if item['type'] == 'default_cards')
+    # Download JSON file
+    print(f"Downloading Card data (JSON) from {default_cards_url} (Could take a bit)")
+    response = requests.get(default_cards_url)
+    return response.json()
+
+# Updates only if there are changes to card data
+def update_cards(json_data):
+    stmt = insert(Card).values(json_data)
+    # Defines what to update
+    update_dict = {
+        'card_name': stmt.excluded.card_name,
+        'rarity': stmt.excluded.rarity,
+        'set_code': stmt.excluded.set_code,
+        'collector_num': stmt.excluded.collector_num,
+        'image_url': stmt.excluded.image_url
+    }
+
+    update_stmt = stmt.on_conflict_do_update(
+        index_elements=['scryfall_id'],
+        set_=update_dict
+    )
+
+    db.session.execute(update_stmt)
+    db.session.commit()
+
+def seed_database():
+    with app.app_context():
+        print("Creating tables if they do not exist...")
+        db.create_all()
+
+        cards_data = fetch_scryfall_data()
+        print(f"Loaded {len(cards_data)} cards from Scryfall. Processing...")
+
+        batch_size = 2000
+        cards_to_insert = []
+
+        for card in cards_data:
+            image_url = None
+            if 'image_uris' in card and 'normal' in card['image_uris']:
+                image_url = card['image_uris']['normal']
+            elif 'card_faces' in card and 'image_uris' in card['card_faces'][0]:
+                image_url = card['card_faces'][0]['image_uris']['normal']
+
+            cards_to_insert.append({
+                'scryfall_id': card.get('id'),
+                'card_name': card.get('name'),
+                'rarity': card.get('rarity'),
+                'set_code': card.get('set'),
+                'collector_num': card.get('collector_number'),
+                'image_url': image_url
+            })
+
+            # Updates cards in batches
+            if len(cards_to_insert) >= batch_size:
+                update_cards(cards_to_insert)
+                cards_to_insert = [] # empty list
+
+        # Catches any remaining cards after for-loop
+        if cards_to_insert:
+            update_cards(cards_to_insert)
+
+        print("Database sync with Scryfall complete!")
+
+if __name__ == "__main__":
+    seed_database()
