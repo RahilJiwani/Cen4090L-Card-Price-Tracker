b17f31c938fadddc71f583ce2b129508c3ca9703
Race11e <rma23f@fsu.edu>
2026-03-03 21:23:01 -0500

    Add User and Watchlist tables to app.py
    User and watchlist tables added to db. Prices will be implemented next.

--- new_app/app.py
+++ new_app/app.py
@@ -1,19 +1,28 @@
 import os
+import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash, check_password_hash
 
+# NOTE: Have .env file in same directory, should look like
+# DATABASE_URL="database@link.com"
+# do not upload database url to github, it is a secret (see settings)
 load_dotenv()
 conn_str = os.getenv('DATABASE_URL')
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+if not conn_str:
+    raise ValueError("No DATABASE_URL found in environment variables. Check your .env file!")
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = conn_str
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 class Card(db.Model):
+    __tablename__ = 'cards'
     card_id = db.Column(db.Integer, primary_key=True)
     scryfall_id = db.Column(db.String(50), unique=True, nullable=False)
     card_name = db.Column(db.String(255), nullable=False)
@@ -22,4 +31,33 @@ class Card(db.Model):
     collector_num = db.Column(db.String(50), nullable=False)
     image_url = db.Column(db.Text, nullable=True)
 
-# Need PriceHistory, User, and Favorites Tables (will be added)
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.today())
+
+    # Many-to-many relationship
+    watchlist = db.relationship('Card', secondary='watchlists', backref='watched_by')
+
+    # Helper funcs==============================================================================
+
+    # Use to hash password
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # Use to authenticate password
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Watchlist(db.Model):
+    __tablename__ = 'watchlists'
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    card_id = db.Column(db.Integer, db.ForeignKey('cards.card_id'), primary_key=True)
+
+
+# Need PriceHistory (will be added)
+
+
