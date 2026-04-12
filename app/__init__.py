606c0657f14ce2df46fcedf45a66db3cf839806f
Hobbs04067 <tjk24c@fsu.edu>
2026-04-12 12:55:34 -0400

    Removed old app.

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Top level package for the MTG price alert project.
-
-This package contains modules for downloading card metadata from MTGJSON,
-loading price data, maintaining a user watchlist, detecting price drops,
-and dispatching email notifications. See README.md for usage instructions.
-"""
